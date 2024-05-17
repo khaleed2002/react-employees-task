133f0eba2ae69f8a11abe67c98efffe423a3fd01
khaleed2002 <zekomadred159@gmail.com>
2024-05-17 15:14:39 +0300

    setup react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
     <>
-      <p>Hello world</p>
+      <Sidebar />
     </>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+
+import './Sidebar.css'
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-item">Dashboard</div>
+      <div className="sidebar-item">Workspace</div>
+      <div className="sidebar-item">Employees</div>
+      <div className="sidebar-item">Holidays</div>
+      <div className="sidebar-item">Inbound Requests</div>
+    </div>
+  )
+}
+export default Sidebar
